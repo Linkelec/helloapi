1fd4f983605c00393c0c6c707e18a64ef7bd78fc
Linkelec <linkelec69@gmail.com>
2024-07-12 12:05:02 +0200

    create profile model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -12,11 +12,14 @@ class Admin extends Authenticatable
     use HasFactory, HasApiTokens, Notifiable;
 
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 
+        'email', 
+        'password',
     ];
 
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 
+        'remember_token',
     ];
 
     protected $casts = [
